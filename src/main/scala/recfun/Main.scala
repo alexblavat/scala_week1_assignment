d72c8a9547500ebfc7da5934854db2d456fa3dce
Oleksiy Blavat <blavat.alex@gmail.com>
2016-06-27 11:46:07 -0400

    Exercise 3 done - money changer

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -18,20 +18,55 @@ object Main {
       1
     else
       pascal(c-1, r-1) + pascal(c, r-1)
-
   }
 
   /**
     * Exercise 2
     */
   def balance(chars: List[Char]): Boolean = {
-    return false
+    if(chars.isEmpty) false
+    else{
+      def isBrace (c: Char): Boolean = {
+        if (c == ')' || c == '(') return true else return false
+      }
+      val cleanList = chars.filter(isBrace _)
+      if (cleanList.head == ')') return false
+      else{
+        def cleanMatch(parenthesesArray: List[Char]): Boolean = {
+          val stack = new scala.collection.mutable.Stack[Char]
+          parenthesesArray.foreach(
+            ch => {
+              if(ch == '(') {
+                stack.push(ch)
+              }else {
+                try {
+                  stack.pop()
+                } catch {
+                  case e: java.util.NoSuchElementException => return false
+                }
+              }
+            }
+          )
+          if(stack.isEmpty) return true else return false
+        }
+        cleanMatch(cleanList)
+      }
+
+    }
   }
 
   /**
     * Exercise 3
     */
   def countChange(money: Int, coins: List[Int]): Int = {
-    return 0
+    def waysToGiveChange(money: Int, coins: List[Int], ways_ct:Int): Int = {
+      if(money < 0) ways_ct
+      else if(coins.isEmpty){
+        if(money == 0) ways_ct + 1 else ways_ct
+      }else{
+        waysToGiveChange(money, coins.tail, ways_ct) + waysToGiveChange(money - coins.head, coins, ways_ct)
+      }
+    }
+    waysToGiveChange(money, coins, 0)
   }
 }
